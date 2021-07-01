5214a5d3b6eb55f4fb9d275018f5786be0f466c6
Ricardo Flores <richard.flowersxx@gmail.com>
2021-07-01 16:11:30 -0400

    Heroku Create

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
-  {
+ "mongodb+srv://Ricardo:Porsche1!@cluster0.zpleq.mongodb.net/b-track?retryWrites=true&w=majority",
+  {git
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
