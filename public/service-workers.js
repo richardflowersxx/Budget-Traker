ed635d8069f96dd8d917f015c26b2fd189bb93b4
Ricardo Flores <richard.flowersxx@gmail.com>
2021-07-09 14:02:03 -0400

    fix the error in service-workers.js

--- public/service-workers.js
+++ public/service-workers.js
@@ -2,10 +2,10 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/index.js',
-    'style.css',
+    'styles.css',
     'db.js',
     '/icons/icon-192x192.png',
-    '/icons/icon-5122x512.png',
+    '/icons/icon-512x512.png',
     'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
   ];
   
